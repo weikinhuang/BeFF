61a7f9b7d41051459be5b6fb0f59342c9c6dd5bf
Alex Lee <aintaer@gmail.com>
2014-05-12 20:44:32 -0400

    View/Entity: default template function
    Uses this.mustache and this.partials.

--- View/Entity.js
+++ View/Entity.js
@@ -1,13 +1,18 @@
 define([
+  'jquery',
   'nbd/View/Entity',
   '../trait/eventMappable'
-], function(Entity, eventMappable) {
+], function($, Entity, eventMappable) {
   'use strict';
 
   return Entity.extend({
     init: function(model) {
       this._super(model);
       this.on('postrender', this._mapEvents);
+    },
+
+    template: function(templateData) {
+      return this.mustache && $(this.mustache(templateData, this.partials));
     },
 
     destroy: function() {

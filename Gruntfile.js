8a1ab65f3425178291293d538881a0c0f292f008
Alex Lee <axal@adobe.com>
2014-05-16 11:51:42 -0400

    Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        ignores: [
+          'Gruntfile.js'
+        ]
+      },
+      test: [
+        '*.js',
+        'Component/**/*.js',
+        'Controller/**/*.js',
+        'View/**/*.js',
+        'trait/**/*.js',
+        'util/**/*.js',
+        'ux/**/*.js'
+      ]
+    },
+    karma: {
+      options: {
+        configFile: 'test/karma.conf.js',
+        singleRun: true
+      },
+      persistent: {
+        singleRun: false
+      },
+      single: {
+        browsers: ['PhantomJS']
+      },
+      multi: {
+        reporters: ['dots'],
+        browsers: ['PhantomJS', 'Firefox'/*, 'Chrome'*/]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('test', ['jshint', 'karma:persistent']);
+  grunt.registerTask('travis', ['jshint', 'karma:multi']);
+  grunt.registerTask('default', ['test']);
+};

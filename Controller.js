d0306f4c0d65c3238f1923571c6f7f7194d41ed1
Alex Lee <axal@adobe.com>
2014-06-05 18:15:58 -0400

    Controller: contained entities in .nests
    .nests should be an object of other entities. The keys of that object
    should match the keys of the view's .nests. The view's .nests is an
    object of selectors, such that matching .nests keys will have the entity
    rendered into the selector.

--- Controller.js
+++ Controller.js
@@ -1,9 +1,10 @@
 define([
   'jquery',
   'nbd/util/extend',
+  'nbd/trait/pubsub',
   'nbd/Controller/Entity',
   './View'
-], function($, extend, Entity, View) {
+], function($, extend, pubsub, View, Entity) {
   'use strict';
 
   return Entity.extend({
@@ -34,12 +35,29 @@ define([
       this._view.$view = $view;
 
       if ($view) {
-        this._view
-        .trigger('postrender', $view)
-        .rendered($view);
+        this._view.trigger('postrender', $view);
       }
+    },
+
+    _initView: function() {
+      this._super.apply(this, arguments);
+      this.listenTo(this._view, 'postrender', this._renderNested);
+    },
+
+    _renderNested: function($view, map) {
+      map = map || this._view.nests;
+      if (!(map && this.nests)) { return; }
+
+      Object.keys(map).forEach(function(key) {
+        var selector = map[key],
+        contained = this.nests[key],
+        $context = $view.find(selector);
+
+        return $context && contained && contained.render($context);
+      }, this);
     }
   }, {
     VIEW_CLASS: View
-  });
+  })
+  .mixin(pubsub);
 });

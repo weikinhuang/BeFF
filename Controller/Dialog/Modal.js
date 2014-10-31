b3a275b03dc10de8db042433091e160fd8f988f7
Alex Lee <aintaer@gmail.com>
2014-10-31 12:06:20 -0400

    Controller/Dialog/Modal: allow extension of promiseGenerator
    promiseGenerator is a good enough concept to let subclasses extend. This
    way the downstream can offer a different Modal.init experience without
    much work.

--- Controller/Dialog/Modal.js
+++ Controller/Dialog/Modal.js
@@ -9,13 +9,14 @@ define([
   var Modal = Dialog.extend({
     switchView: function() {
       this._super.apply(this, arguments);
-      this._view
-      .on('confirm', function() {
-        this.confirm.apply(this, arguments);
-      }, this)
-      .on('hide', function() {
-        this.cancel.apply(this, arguments);
-      }, this);
+      this.listenTo(this._view, {
+        confirm: function() {
+          this.confirm.apply(this, arguments);
+        },
+        hide: function() {
+          this.cancel.apply(this, arguments);
+        }
+      });
     },
 
     confirm: function() {
@@ -31,9 +32,10 @@ define([
     init: function(options, promiseGenerator) {
       var dialog = new this(options),
       destroy = dialog.destroy.bind(dialog);
+      promiseGenerator = promiseGenerator || dialog.promiseGenerator;
 
       function resolveGenerator() {
-        var retval = promiseGenerator();
+        var retval = promiseGenerator.call(dialog);
         if (retval && typeof retval.then === 'function') {
           retval.then(dialog.resolve.bind(dialog));
         }

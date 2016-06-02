dc10d382cbac8a40f32e335566f03f0920ae1107
Wei Kin Huang <weikin.huang04@gmail.com>
2016-06-02 17:04:05 -0400

    Dialog: fire events when the dom can be interacted with

--- test/specs/View/Dialog.js
+++ test/specs/View/Dialog.js
@@ -21,5 +21,37 @@ define([
       view.destroy();
       view._hiding.then(done);
     });
+
+    it('fires the show event when ready to be shown', function(done) {
+      var context = affix('body');
+      var view = new View();
+      view.render(context);
+      view.on('show', done);
+      view.show();
+    });
+
+    it('fires the visible event when actually shown', function(done) {
+      var context = affix('body');
+      var view = new View();
+      view.render(context);
+      view.on('visible', function() {
+        expect(view.$view.is(':visible')).toBe(true);
+        done();
+      });
+      view.show();
+    });
+
+    it('fires the hidden event when closed', function(done) {
+      var context = affix('body');
+      var view = new View();
+      view.render(context);
+      view.on('hidden', function() {
+        expect(view.$view.is(':visible')).toBe(false);
+        done();
+      });
+      view.show();
+
+      view.hide();
+    });
   });
 });

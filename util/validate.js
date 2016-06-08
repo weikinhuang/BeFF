5243e62cb0ed8a784d64c958dd2a60233fb98127
Wei Kin Huang <weikin.huang04@gmail.com>
2016-06-08 15:34:36 -0400

    Fix validation error message

--- util/validate.js
+++ util/validate.js
@@ -127,7 +127,7 @@ define(function() {
     },
     UrlWithProtocol:{
       test: test.bind(/^https?:\/\/(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]*[a-zA-Z0-9])?\.)+(?:(?:[Xx][Nn]--)?[a-zA-Z0-9]{2,63}|\d{1,3})[^<>"]*$/),
-      message: "This field must be a URL starting with http:// or www."
+      message: "This field must be a URL starting with http:// or https://"
     },
     UrlExt:{
       test: test.bind(/^((?:https?):\/\/)?(?:(?:(?:[\w\.\-\+!$&\'\(\)*\+,;=_]|%[0-9a-f]{2})+:)*(?:[\w\.\-\+%!$&\'\(\)*\+,;=]|%[0-9a-f]{2})+@)?(?:[A-Za-z0-9_\-]+\.)(?:[A-Za-z0-9\-\._])+(?::\d+)?(?:[\/|\?](?:[\w#!:\.\?\+=&@$\'~*,;_\/\(\)\[\]\-]|%[0-9a-f]{2})*)?$/),

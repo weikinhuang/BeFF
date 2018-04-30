222ce017258b38ffcbde556cd380ed16946427be
Wei Kin Huang <weikin.huang04@gmail.com>
2018-04-30 14:41:28 -0400

    Add ts typings for ui/trait

---
+++ ui/trait/draggable.d.ts
@@ -0,0 +1,6 @@
+export interface UiDraggableTrait {
+  makeDraggable($view: JQuery, handle?: string, containment?: string, cancelSelector?: string): JQuery;
+}
+
+declare const _default: UiDraggableTrait;
+export default _default;
